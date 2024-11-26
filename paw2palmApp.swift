23091700e7d9d3d97051b88bfbb8b5e7bbc62a97
robinrivpila <110060506+robinrivpila@users.noreply.github.com>
2024-11-25 18:01:21 -0800

    Add files via upload
    maybe this works?

---
+++ paw2palmApp.swift
@@ -0,0 +1,17 @@
+//
+//  paw2palmApp.swift
+//  paw2palm
+//
+//  Created by Robin Pilapil on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct paw2palmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
