7bf8c184710b94d45ec8827236b78177a70710c0
derrick-ng <ng.derrick1@hotmail.com>
2024-12-08 19:08:53 -0800

    test

---
+++ paw2palm/RegisterView.swift
@@ -0,0 +1,7 @@
+//
+//  RegisterView.swift
+//  paw2palm
+//
+//  Created by Derrick Ng on 12/8/24.
+//
+
--- paw2palm/WelcomeView.swift
+++ paw2palm/WelcomeView.swift
@@ -6,15 +6,53 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import FirebaseFirestore
 
+
+// should be in a sep file, AppDelegate
+//class AppDelegate: NSObject, UIApplicationDelegate {
+//    func application(_ application: UIApplication,
+//                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+//        FirebaseApp.configure()
+//        return true
+//    }
+//}
 
 struct WelcomeView: View {
+//    this basically calls AppDelegate
+//    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    init() {
+        initializeFirebase()
+        initializeDatabase()
+    }
+    
     var body: some View {
         VStack{
             TitleComponent()
             Text("Maybe an image here")
             Text("Login")
-            Text("create an account")
+            Button {
+                
+            } label: {
+                Text("Sign Up")
+            }
         }
     }
+}
+
+func initializeFirebase() {
+    //maybe this
+    //https://firebase.google.com/docs/firestore/quickstart
+    if FirebaseApp.app() == nil {
+        FirebaseApp.configure()
+        print("Firebase initialized, success")
+    }
+}
+
+func initializeDatabase() {
+    let db = Firestore.firestore()
+    
+
 }
