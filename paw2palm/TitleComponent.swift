8c20f653e08ed9ea8437c712e7d7d26ebd917e89
derrick-ng <ng.derrick1@hotmail.com>
2024-12-08 16:35:28 -0800

    sample welcome page

---
+++ paw2palm/TitleComponent.swift
@@ -0,0 +1,15 @@
+//
+//  TitleView.swift
+//  paw2palm
+//
+//  Created by Derrick Ng on 12/8/24.
+//
+
+import SwiftUI
+
+struct TitleComponent: View {
+    var body: some View {
+        Text("paw2palm")
+        //add styling
+    }
+}
---
+++ paw2palm/WelcomeView.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomeView.swift
+//  paw2palm
+//
+//  Created by Derrick Ng on 12/8/24.
+//
+
+import SwiftUI
+
+
+struct WelcomeView: View {
+    var body: some View {
+        VStack{
+            TitleComponent()
+            Text("Maybe an image here")
+            Text("Login")
+            Text("create an account")
+        }
+    }
+}
